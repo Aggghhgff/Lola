bbcb40755c7df9ca64b15e0311a4165172572869
Aggghhgff <167721357+Aggghhgff@users.noreply.github.com>
2024-04-23 20:18:53 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 const gameMods = [ 
-    { name: '0.66 mod menu stumble guys', image: 'https://raw.githubusercontent.com/Aggghhgff/Lola/main/download%20(3).jpeg' },
-    { name: 'Mod 2', image: 'https://raw.githubusercontent.com/Aggghhgff/Lola/main/download%20(3).jpeg', downloadLink: 'https://example.com/mod2.zip' },
+    { name: '0.66 mod menu stumble guys', image: 'download (3).jpeg' },
+    { name: 'Mod 2', image: download (3).jpeg', downloadLink: 'https://example.com/mod2.zip' },
     { name: 'Mod 3', image: 'https://raw.githubusercontent.com/Aggghhgff/Lola/main/download%20(3).jpeg', downloadLink: 'https://example.com/mod3.zip' },
     // Add more mods here
 ];
