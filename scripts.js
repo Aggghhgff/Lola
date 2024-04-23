8106ee7dae9532e509fd5e07026e7c177260a18f
Aggghhgff <167721357+Aggghhgff@users.noreply.github.com>
2024-04-23 20:22:26 +0530

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,7 +2,16 @@ const gameMods = [
     { name: '0.66 mod menu stumble guys', image: 'download (3).jpeg' },
     { name: 'Mod 2', image: download (3).jpeg', downloadLink: 'https://example.com/mod2.zip' },
     { name: 'Mod 3', image: 'https://raw.githubusercontent.com/Aggghhgff/Lola/main/download%20(3).jpeg', downloadLink: 'https://example.com/mod3.zip' },
-    // Add more mods here
+    
+
+
+   { name: 'Mod 3', image: 'https://raw.githubusercontent.com/Aggghhgff/Lola/main/download%20(3).jpeg', downloadLink: 'https://example.com/mod3.zip' },
+    
+
+
+
+
+// Add more mods here
 ];
 
 const gameModsContainer = document.getElementById('gameMods');
